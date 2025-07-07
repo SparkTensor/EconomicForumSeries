c7fcb5a464c716e5d49b0108a34e436efc550c43
SparkTensor <Ecudanielsave@gmail.com>
2025-07-07 11:10:57 +0100

    Adding python-dotenv to settings.py file for robust enviroment loading

--- efs_portal/settings.py
+++ efs_portal/settings.py
@@ -15,6 +15,9 @@ from decouple import config
 import dj_database_url
 import os # Add this import
 
+from dotenv import load_dotenv
+load_dotenv()
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
